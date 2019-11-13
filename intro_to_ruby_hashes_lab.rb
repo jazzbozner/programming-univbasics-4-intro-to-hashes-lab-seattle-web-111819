0a79a98820484ba200e70b11738844670de0adab
Jazz Bozner <jazzbozner@gmail.com>
2019-11-13 06:35:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,7 +24,7 @@ end
 
 def update_counting_hash(hash, key)
   if hash[key] = 0
-    hash[key] = 1
+    hash[key] = {key => 1}
   end
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
